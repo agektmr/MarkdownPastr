790ef400e3162db63e4036687d97dfcd39b0bdca
Eiji Kitamura <agektmr@gmail.com>
2014-03-10 13:36:50 +0900

    change welcome message url

--- app/js/background.js
+++ app/js/background.js
@@ -5,7 +5,7 @@ chrome.runtime.onInstalled.addListener(function(details) {
   // Pop up history page only if the version changes in major (ex 2.0.0) or minor (ex 2.1.0).
   // Trivial change (ex 2.1.1) won't popu up.
   if (details.reason == 'install') {
-    chrome.tabs.create({url:'http://demo.agektmr.com/MarkdownPastr/'});
+    chrome.tabs.create({url:'http://demo.agektmr.com/MarkdownPastr/welcome.html'});
   } else if (details.reason === 'update') {
     chrome.tabs.create({url:chrome.runtime.getURL('/HISTORY.html')});
   }
